db272a37c6a7328d1a2c15a178eff900aa2b0e80
Jenkins <juanjo.hernandez@movicoders.com>
2025-02-19 15:28:17 +0100

    Activity from scratch manually created and lifecycle support.

--- app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package es.usj.jjhernandez.mainapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +9,44 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import es.usj.jjhernandez.mainapplication.databinding.ActivityMainBinding
 
+const val TAG = "LOG"
 class MainActivity : AppCompatActivity() {
 
-    val view = ActivityMainBinding.inflate(layoutInflater)
+    private val view by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
-        view.tvContent.text = "Hello Android Class from bindings"
+        Log.v(TAG, "onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(TAG, "onDestroy called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(TAG, "onRestart called")
     }
 }
