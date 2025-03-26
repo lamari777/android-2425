f0b2f7858ff282966c7cf3ee63ab99ee847d79e2
Jenkins <juanjo.hernandez@movicoders.com>
2025-03-26 14:48:40 +0100

    feature/hands-on-3: initial commit

--- app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package es.usj.jjhernandez.mainapplication
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import es.usj.jjhernandez.mainapplication.databinding.ActivityMainBinding
 
@@ -13,12 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
-        view.tvHelloWorld.visibility = View.GONE
-        view.tvHelloWorld.text = getString(R.string.helloworld)
-        view.swComponents.isChecked = false
-        view.swComponents.setOnCheckedChangeListener { _, isChecked ->
-            val visibility = if (isChecked) View.VISIBLE else View.GONE
-            view.tvHelloWorld.visibility = visibility
-        }
     }
 }
