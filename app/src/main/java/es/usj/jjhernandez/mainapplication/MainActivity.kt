57a5d10c754fab97b6a721003ff349abd05e29d9
Jenkins <juanjo.hernandez@movicoders.com>
2025-03-26 14:46:40 +0100

    feature/hands-on-2: View bindings enabled and visibility implemented

--- app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
@@ -1,16 +1,24 @@
 package es.usj.jjhernandez.mainapplication
 
 import android.os.Bundle
-import android.widget.TextView
-
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import es.usj.jjhernandez.mainapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val view by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.tvHelloWorld)
-        textView.text = getString(R.string.helloworld)
+        setContentView(view.root)
+        view.tvHelloWorld.visibility = View.GONE
+        view.tvHelloWorld.text = getString(R.string.helloworld)
+        view.swComponents.isChecked = false
+        view.swComponents.setOnCheckedChangeListener { _, isChecked ->
+            val visibility = if (isChecked) View.VISIBLE else View.GONE
+            view.tvHelloWorld.visibility = visibility
+        }
     }
 }
