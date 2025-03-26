3d1fb96ec7f9306e3dc59cabfb1a2f060877b460
Jenkins <juanjo.hernandez@movicoders.com>
2025-03-26 15:15:33 +0100

    feature/hands-on-5-ex1: Dynamic items listView

--- app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
@@ -1,16 +1,22 @@
 package es.usj.jjhernandez.mainapplication
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import es.usj.jjhernandez.mainapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val items by lazy {
+        (1..1000).map { "Item $it" }
+    }
     private val view by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
+        view.ltCountries.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
     }
 }
