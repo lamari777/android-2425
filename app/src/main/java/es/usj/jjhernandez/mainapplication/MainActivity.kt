d394e2563f952524af6a3afb431dd2c426f14b51
Jenkins <juanjo.hernandez@movicoders.com>
2025-03-26 16:11:37 +0100

    feature/hands-on-5-ex4: List with custom adapter and model.

--- app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
+++ app/src/main/java/es/usj/jjhernandez/mainapplication/MainActivity.kt
@@ -1,35 +1,80 @@
 package es.usj.jjhernandez.mainapplication
 
+import android.app.Activity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import es.usj.jjhernandez.mainapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private val items by lazy {
-        (1..1000).map { "Item $it" }
-    }
-
     private val view by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val items by lazy {
+        (1 .. 1000).map { Item("Name $it", "Surname $it") }
+    }
+
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(view.root)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        val adapter = CustomAdapter(items)
         view.ltCountries.adapter = adapter
-        view.ltCountries.setOnItemClickListener { _, _, position, _ ->
-            val elementToRemove = items[position]
-            adapter.remove(elementToRemove)
+        view.ltCountries.setOnItemClickListener{ _, _, position, _ ->
+            val element_to_remove = items[position]
+            // adapter.remove(element_to_remove)
             adapter.notifyDataSetChanged()
-            Toast.makeText(this, "$elementToRemove removed", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Item $element_to_remove removed", Toast.LENGTH_SHORT).show()
         }
-        view.fabAdd.setOnClickListener {
-            adapter.add("New Item ${items.size + 1}")
+
+        view.fbtnAdd.setOnClickListener {
+            // adapter.add("New item: ${items.size + 1}")
             adapter.notifyDataSetChanged()
         }
+    }
+}
+
+class CustomAdapter(var items: List<Item>) : BaseAdapter() {
+    override fun getCount() : Int {
+        return items.size
+    }
+
+    override fun getItem(position : Int) : Item {
+        return items[position]
+    }
+
+    override fun getItemId(position : Int) : Long {
+        return position.toLong()
+    }
+
+    override fun getView(
+        position : Int,
+        convertView : View?,
+        parent : ViewGroup?
+    ) : View {
+        val item = getItem(position)
+        val view = LayoutInflater.from(
+            parent?.context
+                ?: Activity()
+        ).inflate(R.layout.single_item_layout, parent,
+            false)
+        val txt_1 = view.findViewById<TextView>(R.id.txt_1)
+        val txt_2 = view.findViewById<TextView>(R.id.txt_2)
+        txt_1.text = item.name
+        txt_2.text = item.surname
+        return view
+    }
+
+}
+
+data class Item(var name: String, var surname: String) {
+
+    override fun toString(): String {
+        return "$name $surname"
     }
 }
